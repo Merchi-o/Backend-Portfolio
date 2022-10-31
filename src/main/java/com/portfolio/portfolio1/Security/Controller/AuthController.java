0caccc946bbd74e78fc0c5e685909c62762d7e0b
Merchi-o <martinaorellana90@gmail.com>
2022-10-30 22:00:45 -0300

    corrigiendo CORSE en el back

--- src/main/java/com/portfolio/portfolio1/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/portfolio1/Security/Controller/AuthController.java
@@ -34,8 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https:// .web.app")
+@CrossOrigin(origins = "https://frontendmartinaorellana.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
